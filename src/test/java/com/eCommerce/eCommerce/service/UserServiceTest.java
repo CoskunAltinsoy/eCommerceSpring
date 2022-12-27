75ff92978eba8770b777cb8b261f25ef4b521e8a
Coşkun Altınsoy <coskun.altinsoyy@gmail.com>
2022-12-27 11:44:07 +0300

    testing fixed

--- src/test/java/com/eCommerce/eCommerce/service/UserServiceTest.java
+++ src/test/java/com/eCommerce/eCommerce/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.eCommerce.eCommerce.dto.UserDto;
 import com.eCommerce.eCommerce.model.User;
 import com.eCommerce.eCommerce.repository.UserRepository;
 import com.eCommerce.eCommerce.service.converter.UserConverter;
@@ -30,9 +31,17 @@ class UserServiceTest extends TestSupport{
 	@Test
 	void testGetAllUsers_itShouldReturnUserDtoList() {
 		List<User> userList = generateUsers();
+		List<UserDto> userDtoList = generateUserDtoList(userList);
 		
 		Mockito.when(userRepository.findAll()).thenReturn(userList);
 		Mockito.when(userConverter.convert(userList)).thenReturn(generateUserDtoList(userList));
+		
+		List<UserDto> result = userService.getAllUsers();
+		
+		assertEquals(result, userDtoList);
+		
+		Mockito.verify(userRepository).findAll();
+		Mockito.verify(userConverter).convert(userList);
 	
 	}
 
